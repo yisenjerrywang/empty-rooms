fcb24d8309bf0d328a6b384ab35a445e0203e65e
Jerry Wang <yisenjerrywang@gmail.com>
2015-08-18 21:45:50 -0400

    Room as URL path parameters

--- routes/times.js
+++ routes/times.js
@@ -6,10 +6,10 @@ var apiToken = '8f299783944ad67bf01956c978853984';
 var uwapi = require('uwapi')(apiToken);
 
 /* GET times page. */
-router.get('/', function(req, res, next) {	
-	uwapi.buildingsCourses({'building_acronym': "RCH", 'room_number' : 302} ).then(function(buildings) {
+router.get('/:building/:room_number', function(req, res, next) {
+	uwapi.buildingsCourses({'building_acronym': req.params.building, 'room_number' : req.params.room_number} ).then(function(buildings) {
 		findEmptyTime(buildings, function(times) {
-			res.render('times', { times: times });
+			res.render('times', { times: times , building : req.params.building, room_number: req.params.room_number});
 		});
 	});
 });
