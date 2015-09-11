554710bc642daaaf16d350e9ba10bd937565c202
Jerry Wang <yisenjerrywang@gmail.com>
2015-09-10 22:22:04 -0400

    Times route sends list as response

--- public/js/angularApp.js
+++ public/js/angularApp.js
@@ -1,5 +1,17 @@
-function QuickView () {
+function QuickView ($http) {
 	this.curBuilding = "";
+	this.curRoomNum = "";
+	this.getTimes = function() {
+		$http.get('http://localhost:3000/times/' + this.curBuilding + '/' + this.curRoomNum).then(
+		// Success
+		function(response) {
+			alert(response.data);
+		},
+		// Error
+		function(response) {
+			alert("ERROR");
+		})
+	}
 }
 
 angular
--- routes/times.js
+++ routes/times.js
@@ -9,7 +9,9 @@ var uwapi = require('uwapi')(apiToken);
 router.get('/:building/:room_number', function(req, res, next) {
 	uwapi.buildingsCourses({'building_acronym': req.params.building, 'room_number' : req.params.room_number} ).then(function(buildings) {
 		findEmptyTime(buildings, function(times) {
-			res.render('times', { times: times , building : req.params.building, room_number: req.params.room_number});
+			res.send(times);
+			res.end();
+			//res.render('times', { times: times , building : req.params.building, room_number: req.params.room_number});
 		});
 	});
 });
